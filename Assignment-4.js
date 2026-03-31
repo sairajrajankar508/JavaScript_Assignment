22bdcebbeee186fe5b73bbbd172e74d4a4fc73df
Sairaj Rajankar <sairajrajankar508@gmail.com>
2026-03-31 10:15:59 +0530

    Delete Assignment-4.js

--- Assignment-4.js
+++
@@ -1,8 +0,0 @@
-let isMember = true;
-let cartValue = 1500;
-
-if ((isMember && cartValue > 1000) || cartValue > 2000) {
-    console.log("You are eligible for free delivery!");
-} else {
-    console.log("You are not eligible for free delivery.");
-}
